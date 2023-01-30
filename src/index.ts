9d61cfcf7e382a09077fd22365aebac6878e6c25
Frank_Udoags <frankudoags@gmail.com>
2023-01-30 08:34:37 +0100

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import "./db/index";
+import dummyRouter from "./routes/dummyRoute";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//dummy Route
+app.use("/dummy", dummyRouter);
